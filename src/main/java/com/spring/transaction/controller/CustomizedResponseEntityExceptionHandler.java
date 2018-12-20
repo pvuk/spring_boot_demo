3434a2494fbbb45e2c661c5bfd3d7b8d63088e8c
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2018-12-20 18:28:33 +0530

    Handling all the exceptions in Spring Rest

---
+++ src/main/java/com/spring/transaction/controller/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.spring.transaction.controller;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.spring.transaction.exception.ErrorDetails;
+import com.spring.transaction.exception.NotFoundException;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(NotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(NotFoundException ex, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ErrorDetails> handleUserException(Exception ex, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
