91d95f55306cdc6044edebfa69efdeae944b0df4
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-01-11 17:51:05 +0530

    minor changes

--- src/main/java/com/spring/transaction/model/CreditCard.java
+++ src/main/java/com/spring/transaction/model/CreditCard.java
@@ -43,5 +43,7 @@ public class CreditCard {
 	
 	private Date cardIssuedOn;
 	
+	private Date cardExpiredOn;
+	
 	private Long rewardCardId;
 }
--- src/main/java/com/spring/transaction/repository/mongo/BankMongoRepository.java
+++ src/main/java/com/spring/transaction/repository/mongo/BankMongoRepository.java
@@ -1,5 +1,7 @@
 package com.spring.transaction.repository.mongo;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.spring.transaction.model.Bank;
@@ -7,5 +9,5 @@ import com.spring.transaction.model.Bank;
 //No need implementation, just one interface, and you have CRUD, thanks Spring Data
 public interface BankMongoRepository extends MongoRepository<Bank, Long>{
 
-	Bank findByBankName(String bankName);
+	List<Bank> findAllByBankName(String bankName);
 }
