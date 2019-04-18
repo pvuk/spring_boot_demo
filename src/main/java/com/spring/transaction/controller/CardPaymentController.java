29aa9b27aeee00bf2b0ba43b38dfc8924250ad57
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-04-18 19:30:43 +0530

    initial commit

---
+++ src/main/java/com/spring/transaction/controller/CardPaymentController.java
@@ -0,0 +1,11 @@
+package com.spring.transaction.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/cardPayment/custom/")
+public class CardPaymentController {
+	
+	
+}
