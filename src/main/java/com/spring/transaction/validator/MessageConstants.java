c47422fc1d7c9200def7b82439781bf66225ebf3
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-05-24 18:22:05 +0530

    new changes

--- src/main/java/com/spring/transaction/validator/MessageConstants.java
+++ src/main/java/com/spring/transaction/validator/MessageConstants.java
@@ -5,4 +5,7 @@ public interface MessageConstants {
 	public static final String SUCCESS_SAVE 	= "Record Saved Successfully.";
 	public static final String SUCCESS_UPDATE 	= "Record Updated Successfully.";
 	public static final String SUCCESS_DELETE 	= "Record Deleted Successfully.";
+	public static final String ERROR_MESSAGE 	= "Something went wrong. Please Contact Trans IT Support.";
+	public static final String PLEASE_CONTACT_TRANS_IT_SUPPORT = "Please Contact Trans IT Support.";
+	public static final String WORKING_IN_PROGRESS = "Working in progress. ";
 }
