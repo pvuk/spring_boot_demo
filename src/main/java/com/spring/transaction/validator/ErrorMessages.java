fbe1870db85b15c5c26519aa22e42d89e2948c5a
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-05-31 14:29:45 +0530

    Message reponse changes

--- src/main/java/com/spring/transaction/validator/ErrorMessages.java
+++
@@ -1,8 +0,0 @@
-package com.spring.transaction.validator;
-
-public class ErrorMessages {
-	
-	public static final String UPDATE_FAILED = "Update can't done. ";
-	
-	public static final String PLEASE_CONTACT_IT_SUPPORT = "Please contact IT support.";
-}
