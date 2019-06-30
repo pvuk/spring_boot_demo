dc40f3055ea1cd6fd6f950aa0958e275a5750de7
udaykiran pulipati <udaykiran pulipati@DESKTOP-4MF8HRC>
2019-06-30 23:47:53 +0530

    Global exception handler changes

--- src/main/java/com/spring/transaction/controller/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/spring/transaction/controller/CustomizedResponseEntityExceptionHandler.java
@@ -2,19 +2,20 @@ package com.spring.transaction.controller;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.spring.transaction.exception.ErrorDetails;
 import com.spring.transaction.exception.NotFoundException;
-import com.spring.transaction.model.TransErrorLogReport;
-import com.spring.transaction.repository.TransErrorLogReportRepo;
 import com.spring.transaction.validator.MessageConstants;
 
 import lombok.extern.slf4j.Slf4j;
@@ -30,14 +31,19 @@ import lombok.extern.slf4j.Slf4j;
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
 //	@Autowired TransErrorLogReportRepo transErrorLogReportRepo;
-	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		log.error("handleExceptionInternal: {}", getErrorDetails(ex, ex.getMessage(), status, null, request));
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
 	@ExceptionHandler(NotFoundException.class)
 	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(NotFoundException ex, WebRequest request) {
 		HttpStatus httpStatus = HttpStatus.NOT_FOUND;
 		Date date = new Date();
 		String message = ex.getMessage() + ". " + MessageConstants.PLEASE_CONTACT_TRANS_IT_SUPPORT;
 
-		log.error("handleUserNotFoundException: {}", getErrorDetails(ex, message, httpStatus, date));
+		log.error("handleUserNotFoundException: {}", getErrorDetails(ex, message, httpStatus, date, request));
 
 		ErrorDetails errorDetails = new ErrorDetails(date, message, request.getDescription(false));
 
@@ -50,14 +56,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		Date date = new Date();
 		String message = ex.getMessage() + ". " + MessageConstants.PLEASE_CONTACT_TRANS_IT_SUPPORT;
 
-		log.error("handleUserException: {}", getErrorDetails(ex, message, httpStatus, date));
+		log.error("handleUserException: {}", getErrorDetails(ex, message, httpStatus, date, request));
 
 		ErrorDetails errorDetails = new ErrorDetails(date, message, request.getDescription(false));
 
 		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
-	private Object getErrorDetails(Exception ex, String message, HttpStatus httpStatus, Date date) {
+	private Object getErrorDetails(Exception ex, String message, HttpStatus httpStatus, Date date, WebRequest request) {
 		StackTraceElement ste = ex.getStackTrace()[0];
 		String className = ste.getClassName();
 		String methodName = ste.getMethodName();
@@ -65,9 +71,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		String fileName = ste.getFileName();
 		int statusCode = httpStatus.value();
 		String statusName = httpStatus.name();
-		
+		HttpServletRequest httpServletRequest = ((ServletWebRequest) request).getRequest();
+		String requestURL = httpServletRequest.getRequestURL().toString();
+		String queryString = httpServletRequest.getQueryString();
+		queryString = queryString != null ? ", Parameters: "+ queryString : "";
 		Object errorDetails = "Error: " + message + ", occured at class :" + className + ", methodName: " + methodName
-				+ ", lineNumber: " + lineNumber + ", fileName: " + fileName +", StatusCode: "+ statusCode +", StatusName: "+ statusName;
+				+ ", lineNumber: " + lineNumber + ", fileName: " + fileName + ", StatusCode: " + statusCode
+				+ ", StatusName: " + statusName +", requestURL: "+ requestURL + queryString;
 		
 //		TransErrorLogReport logReport = TransErrorLogReport.builder()
 //				.className(className)
@@ -77,7 +87,9 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 //				.lineNumber(lineNumber)
 //				.message(message)
 //				.methodName(methodName)
+//				.parameters(parameters)
 //				.printStackTrace(ex)
+//				.requestURL(requestURL)
 //				.statusCode(statusCode)
 //				.statusName(statusName)
 //				.build();
