16a1930474c54f2de4fa4327518c6f40c1d1e06c
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-07-04 16:37:40 +0530

    Revert "Rewards Redeem Added date changes"
    This reverts commit 34ed139605eeabc70a624fc55888007dd3e13ce1.

--- src/main/java/com/spring/transaction/model/RewardPoints.java
+++ src/main/java/com/spring/transaction/model/RewardPoints.java
@@ -57,8 +57,8 @@ public class RewardPoints {
 	
 	@Field(value="REWARDS_CLOSING_BALANCE", order = 8) private long rewardsClosingBalance;
 	
-	@Field(value="REWARDS_EARNED_ON", order = 9)
-	private Date rewardsEarnedOn;
+	@Field(value="REWARD_POINTS_ADDED_ON", order = 9)
+	private Date rewardPointsAddedOn;
 	
 	@Field(value="MERCHANT_DETAILS_ID", order = 10)
 	@NotEmpty(message = "Merchant Name is required field.")
