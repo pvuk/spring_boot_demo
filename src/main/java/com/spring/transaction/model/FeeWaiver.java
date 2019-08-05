851b5c397d0c605d2944bf25f1670aaaf5b270ec
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-08-05 18:42:11 +0530

    MongoDB support package updated to Spring @Transient. For not saving @Transient variable data into database

--- src/main/java/com/spring/transaction/model/FeeWaiver.java
+++ src/main/java/com/spring/transaction/model/FeeWaiver.java
@@ -3,10 +3,10 @@ package com.spring.transaction.model;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
