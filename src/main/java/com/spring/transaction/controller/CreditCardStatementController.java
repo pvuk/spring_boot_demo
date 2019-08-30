829208fde95c0e5ab597e59a83cad522ef8b3382
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-08-30 19:09:59 +0530

    Upload Multiple statements new service. Ref: http://bit.ly/2ZEuJcE

--- src/main/java/com/spring/transaction/controller/CreditCardStatementController.java
+++ src/main/java/com/spring/transaction/controller/CreditCardStatementController.java
@@ -1,5 +1,6 @@
 package com.spring.transaction.controller;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,8 @@ import com.spring.transaction.service.CreditCardStatementService;
 @RequestMapping(value = "/creditCardStatement/custom")
 public class CreditCardStatementController {
 	
+	public static final String uploadingDir = System.getProperty("user.dir") + "/statements/uploadDir/";
+	
 	@Autowired private CreditCardStatementService ccStatementService;
 	
 	@PostMapping(value="/uploadStatement/{password}", consumes = {"multipart/form-data"})
@@ -27,5 +30,15 @@ public class CreditCardStatementController {
 			@PathVariable(name = "password") String password) throws IOException {
 		CreditCardStatement ccStatement = ccStatementService.uploadStatement(file, password);
 		return new ResponseEntity<Object>(ccStatement, HttpStatus.OK);
+	}
+	
+	@PostMapping(value="/uploadStatements/{password}", consumes = {"multipart/form-data"})
+	public @ResponseBody String uploadStatements(@RequestParam(name = "files") MultipartFile[] files,
+			@PathVariable(name = "password") String password) throws IOException {
+		for(MultipartFile uploadedFile : files) {
+            File file = new File(uploadingDir + uploadedFile.getOriginalFilename());
+            uploadedFile.transferTo(file);
+        }
+		return "redirect:/";
 	}
 }
