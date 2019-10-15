4a62e25eb6ef6be90db2708bb9febb3611b8353d
Priyanka <Priyanka@LAPTOP-OJ9GMP1L>
2019-10-15 23:12:42 +0530

    Initial commit

---
+++ src/main/java/com/spring/transaction/model/NetBanking.java
@@ -0,0 +1,42 @@
+package com.spring.transaction.model;
+
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "NET_BANKING_CODE")
+public class NetBanking {
+	
+	@Field(value = "NET_BANKING_ID", order = 1)
+	@Id 												private String netBankingId;
+
+	/*
+	 * https://docs.mongodb.com/manual/core/index-unique/
+	 */
+	@Field(value="CODE", order = 2)
+	@Indexed(unique=true)								private String code;
+	
+	@Field(value="POSITION", order = 3) 				private Long position;
+	
+	@NotNull(message="Bank Name is required field.")
+	@Field(value="DESCRIPTION", order = 4) 				private String description;
+	
+	@Field(value="IS_ACTIVE", order = 5) 				private boolean isActive;
+	
+	@Transient
+	@Builder.Default
+	private ErrorMessageMap errorMessageMap = new ErrorMessageMap();
+}
