457c510d719d735c16d1f6891825a501239d5f73
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2020-02-04 18:25:48 +0530

    PAY_TYPE_CODE Initial Commit

---
+++ src/main/java/com/spring/transaction/model/PayType.java
@@ -0,0 +1,32 @@
+package com.spring.transaction.model;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "PAY_TYPE_CODE")
+public class PayType {
+
+	@Id
+	@Field(value = "PAY_TYPE_ID")						private String payTypeId;
+	
+	@Indexed(unique=true)
+	@Field(value="CODE")								private String code;
+	
+	@Field(value="POSITION") 							private Long position;
+	
+	@NotNull(message="Pay Type is required field.")
+	@Field(value="DESCRIPTION") 						private String description;
+}
